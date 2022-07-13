afbbfa1bac729239322434252802c2d0e3c7218b
Mananlearn <85214970+Mananlearn@users.noreply.github.com>
2022-07-13 14:09:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ apt install -y python3.8
 apt install -y software-properties-common
 apt install -y supervisor
 add-apt-repository ppa:deadsnakes/ppa -y
+apt-get -f install -y
 cd ~/
 git clone https://github.com/freqtrade/freqtrade.git
 cd ~/freqtrade/
@@ -17,18 +18,18 @@ cd /user_data/strategies/
 curl -o Apollo11.py https://raw.githubusercontent.com/shanejones/goddard/main/Apollo11.py
 cd ~/freqtrade/
 curl -o config.json https://raw.githubusercontent.com/Mananlearn/Temp1/master/config.json
-cd /etc/supervisor/conf.d/
-curl -o bot1.conf https://raw.githubusercontent.com/Mananlearn/Temp1/master/bot1.conf
-sed -i "s/manan/"$USER"/g" bot1.conf
-supervisorctl reread
 while true
 do
-      read -r -p "Change the config.json file?(No - Dry Run) [Y/n] " input
+      read -r -p "Enter your API key [Y/n] " input
  
       case $input in
             [yY][eE][sS]|[yY])
-                  echo "Yes"
-                  nano config.json
+                  echo "Here you go"
+                  read -sp 'Key: ' uservar
+                  read -sp 'Secret ' passvar
+                  sed -i "s/hahaky/"$uservar"/g" config.json
+                  sed -i "s/hahasc/"$passvar"/g" config.json
+                  sed -i "s/hahadry/true/g" config.json                  
                   break
                   ;;
             [nN][oO]|[nN])
@@ -40,5 +41,28 @@ do
                   ;;
       esac      
 done
+cd /etc/supervisor/conf.d/
+curl -o bot1.conf https://raw.githubusercontent.com/Mananlearn/Temp1/master/bot1.conf
+sed -i "s/manan/"$USER"/g" bot1.conf
+supervisorctl reread
 supervisorctl update
 supervisorctl status
+while true
+do
+      read -r -p "Reboot? [Y/n]" input
+ 
+      case $input in
+            [yY][eE][sS]|[yY])
+                  echo "Yes"
+                  reboot
+                  break
+                  ;;
+            [nN][oO]|[nN])
+                  echo "No"
+                  break
+                  ;;
+            *)
+                  echo "Invalid input..."
+                  ;;
+      esac      
+done
