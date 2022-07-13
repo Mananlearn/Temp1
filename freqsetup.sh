90ca3fabd82a8bf845c0a3d93bb5a70a098f7b84
Mananlearn <85214970+Mananlearn@users.noreply.github.com>
2022-07-13 13:40:09 +0530

    Update freqsetup.sh

--- freqsetup.sh
+++ freqsetup.sh
@@ -19,25 +19,26 @@ cd ~/freqtrade/
 curl -o config.json https://raw.githubusercontent.com/Mananlearn/Temp1/master/config.json
 cd /etc/supervisor/conf.d/
 curl -o bot1.conf https://raw.githubusercontent.com/Mananlearn/Temp1/master/bot1.conf
+sed -i "s/manan/"$USER"/g" bot1.conf
 supervisorctl reread
 while true
 do
-      read -r -p "Are You Sure? [Y/n] " input
+      read -r -p "Change the config.json file?(No - Dry Run) [Y/n] " input
  
       case $input in
             [yY][eE][sS]|[yY])
                   echo "Yes"
+                  nano config.json
                   break
                   ;;
             [nN][oO]|[nN])
                   echo "No"
-                  continue
+                  break
                   ;;
             *)
                   echo "Invalid input..."
                   ;;
       esac      
 done
-nano config.json
 supervisorctl update
 supervisorctl status
